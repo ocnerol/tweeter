b2e2a65a4e035279538c9e3409180bbc391357f2
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-15 02:13:05 +0000

    Log the length of the input value

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   const $tweetFormTextArea = $('#tweet-text');
   
   $tweetFormTextArea.on('input', function(event) {
-    console.log($(this).val());
+    console.log($(this).val().length);
   });
 
 });
