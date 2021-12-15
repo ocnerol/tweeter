be8f684f2e9b1af1b6044daaf78e8bd64d6c5d6d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-15 02:59:39 +0000

    Add negative-value class when counter value is negative, and remove negative-value class if it was added previously and if the value is currently positive

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,8 +6,14 @@ $(document).ready(function() {
   $tweetFormTextArea.on('input', function(event) {
     const inputValueLength = $(this).val().length;
     const $counter = $(this).siblings().children('.counter');
-    
+
     $counter.text(maxCharacters - inputValueLength);
+    if ($counter.text() < 0) {
+      $counter.addClass('negative-value');
+    }
+    if ($counter.hasClass('negative-value') && $counter.text() >= 0) {
+      $counter.removeClass('negative-value');
+    }
   });
 
 });
