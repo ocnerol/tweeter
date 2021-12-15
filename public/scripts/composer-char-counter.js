6af82b91f7bb113442d78aa7e17dfb95244dd08f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-15 01:14:47 +0000

    add input handler to log the value of the input each time there is new input (including deletions)

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  const $tweetFormTextArea = $('#tweet-text');
+  
+  $tweetFormTextArea.on('input', function(event) {
+    console.log($(this).val());
+  });
+
+});
